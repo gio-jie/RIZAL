32213fc21508c49fcfd2e87d1ae49150538235d2
Jerielogy <121427234+Jerielogy@users.noreply.github.com>
2025-12-12 10:05:23 +0800

    added animations for rizal and guards level 1 and 2

--- Assets/Level 1_Assets/RizalMovement.cs
+++ Assets/Level 1_Assets/RizalMovement.cs
@@ -1,31 +1,44 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RizalMovement : MonoBehaviour
 {
     public float speed = 5f;
-
-    // Reference to the Joystick script we made earlier
     public VirtualJoystick joystick;
 
     private Rigidbody2D rb;
+    private Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        // Safety check: make sure joystick is assigned
         if (joystick == null) return;
 
-        // 1. Get Input from Joystick script
         Vector2 moveInput = joystick.InputDirection;
 
-        // 2. Move Rizal (the player) using Physics
         if (moveInput != Vector2.zero)
         {
+            // 1. Move
             rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
+
+            // 2. Rotate
+            float angle = Mathf.Atan2(moveInput.y, moveInput.x) * Mathf.Rad2Deg;
+            rb.rotation = angle - 90f;
+
+            // 3. Animate
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            // --- MOVEMENT STOPPED ---
+            anim.SetBool("isRunning", false);
+
+            // KILL SPINNING MOMENTUM
+            rb.angularVelocity = 0f;
         }
     }
 }
